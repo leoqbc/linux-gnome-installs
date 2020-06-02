5fa680e590c03940cf239eeb986ffd91195b6824
Leonardo Tumadjian <tumadjian@gmail.com>
2020-06-01 22:27:25 -0300

    Add install vscode

--- vscodeinstall.sh
+++ vscodeinstall.sh
@@ -8,6 +8,18 @@ mkdir -p $PACKAGES
 
 wget https://update.code.visualstudio.com/latest/linux-x64/stable -O $PACKAGES/vscode.stable.tar.gz 
 
-mkdir -p $INSTALLS/Code
+mkdir -p $INSTALLS
 
 tar -xzvf $PACKAGES/vscode.stable.tar.gz -C $INSTALLS
+
+touch $HOME/.local/share/applications/Code.desktop
+
+cat > $HOME/.local/share/applications/Code.desktop <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Name=Visual Studio Code
+Exec=$HOME/installs/VSCode-linux-x64/bin/code
+Icon=$HOME/installs/VSCode-linux-x64/resources/app/resources/linux/code.png
+Type=Application
+Categories=Development;
+EOF
